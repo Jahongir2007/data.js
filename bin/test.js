38b16a0f22ea553957d4d9d18e68d4f9870868c8
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-04-29 21:54:39 +0500

    Create test.js

---
+++ bin/test.js
@@ -0,0 +1,14 @@
+const array = require('./data');
+const app = new array(
+    data = ["Data.js",12,14,55,63],
+    get = true,
+    ofArray = {
+        get: 2,
+        leng: false,
+        sort: false,
+        constructor: true
+    },
+    concat = ["success","failed"],
+    copy = 3,
+    entries = true,
+); 
