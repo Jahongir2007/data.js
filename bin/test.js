ed21f77cf8b02e912be86798029987e545e9c23a
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-05-01 17:39:19 +0500

    Update test.js

--- bin/test.js
+++ bin/test.js
@@ -1,14 +1,15 @@
-const data = require('./data');
-const app = new data(
-    data = ["Data.js",12,14,55,63],
-    get = true,
-    ofArray = {
-        get: 2,
-        leng: false,
-        sort: false,
-        constructor: true
-    },
-    concat = ["success","failed"],
-    copy = 3,
-    entries = true,
-); 
+const Data = require("./data");
+var app = new Data(["data.js","javascript",14,25,9,16],{
+    get: 0,
+    getLength: true,
+    join: ", ",
+    copy: 1,
+    constructor: true,
+    entries: true,
+    reverse: true,
+    sort: true,
+    fill: true,
+    shift: true,
+    map: Math.sqrt(),
+    pop: true
+});
